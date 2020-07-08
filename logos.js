badab6738664016cb82328540aa213dc8a356613
fgstaffa <fgstaffa@hotmail.com>
2020-07-07 23:57:03 -0300

    finalizado pagina principal, precisa alterar o nome de formacao para home

---
+++ logos.js
@@ -0,0 +1,23 @@
+let logos = [
+    {
+        img:"../imagens/html5new.png",
+        p: 'Conhecimento intermediário'
+    },
+    {
+        img:"../imagens/css3new.png",
+        p: 'Conhecimento intermediário'
+    },
+    {
+        img:"../imagens/javascriptnew.png",
+        p: 'Estudando atualmente'
+    },
+    {
+        img:"../imagens/php.png",
+        p: 'Conhecimento Básico'
+    },
+    {
+        img:"../imagens/mysqlnew.png",
+        p: 'Conhecimento Básico'
+    }
+]
+
